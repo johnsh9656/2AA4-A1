9002bb7dfb32b514fc0668225b4f6569fca4cf56
johnsh9656 <harrisonjohns1219@gmail.com>
2025-04-03 21:59:45 -0400

    A3: set up junit testing environment

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/SolveMazeTests.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolveMazeTests {
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1+1);
+    }
+}
